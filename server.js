42d03b144b50762b45e4a05e9519a3a408c56626
Fisiha <fishbrnd@gmail.com>
2025-12-30 13:47:24 +0300

    making a good css for an update.ejs , home.ejs and fixing a logout problems

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-
+// declaring blogs
 let blogs = [];
 let id = 1;
+
+// declaring user array for signin
+
+let users = [];
+
  // requiest handler 
 
 app.use(express.urlencoded({extended:true}));
@@ -13,19 +18,22 @@ app.set('view engine', 'ejs')
 // accessing an ejs file from server page & send blogs to an ejs 
 
 app.get('/', (req,res) => {
-    res.render('home',{blogs:blogs});
+    res.render('sign_in',{blogs:blogs});
 })
-
+// access home page 
+app.get('/home',(req,res)=>{
+    res.redirect('home')
+})
 app.post('/post',(req,res)=>{
    blogs.push({id:id++,Btitle:req.body.Btitle,desc:req.body.desc,author:req.body.author,cate:req.body.cate,date:req.body.date});
-   res.redirect('/')
+   res.render('home',{blogs})
 })
 
  // delete button
  app.post('/delete/:id',(req,res)=>{
     const id = parseInt(req.params.id)
     blogs = blogs.filter(blog => blog.id !== id)
-    res.redirect('/')
+    res.render('home',{blogs})
  })
 
  // update button
@@ -40,14 +48,44 @@ app.post('/post',(req,res)=>{
         blog.cate = req.body.cate;
         blog.date = req.body.date;
     }
-    res.redirect('/')
+    res.render('home',{blogs})
  })
  
-/* app.get('/edit/:id', (req, res) => {
-  const blog = blogs.find(b => b.id == req.params.id);
-  res.render('edit', { blog });
-});*/
 
+
+// for edit redirction to update page 
+
+app.get('/edit/:id', (req, res) => {
+    const id = Number(req.params.id);  
+  const blog = blogs.find(e => e.id == req.params.id);
+   if (!blog) return res.redirect('/');
+  res.render('update', { blog });
+});
+
+// getting sign in page
+ app.post('/sign_in/' , (req,res)=>{
+    const {email , password} = req.body;
+    if(email,password){
+        users.push({email,password})
+        res.render('login');
+    }
+ })
+
+  // login 
+
+  app.post('/login',(req,res)=>{
+    const {email,password} = req.body;
+    const user = users.find(u => u.email === email && u.password === password);
+    if(user){
+        res.render('home',{blogs})
+    }else{
+        res.send('the password did not match')
+    }
+  })
+
+  app.get('/logout',(req,res)=>{
+    res.redirect('/')
+  })
 
 app.listen(3000,(req,res)=>{
     console.log('server is on live')
